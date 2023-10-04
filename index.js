3e902cb7719df8397ce93ea26282afe2d719fa53
Nithin177 <nithinshetty172001@gmail.com>
2023-10-04 22:28:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let count = 0;
+document.getElementById("decrease").onclick = function () {
+  count -= 1;
+  document.getElementById("countlable").innerHTML = count;
+};
+document.getElementById("reset").onclick = function () {
+  count = 0;
+  document.getElementById("countlable").innerHTML = count;
+};
+document.getElementById("increase").onclick = function () {
+  count += 1;
+  document.getElementById("countlable").innerHTML = count;
+};
